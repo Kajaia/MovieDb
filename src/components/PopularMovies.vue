7cce65eef29d7a26f049f7bfce64474635884441
Kajaia <lashakajaia31@gmail.com>
2021-07-09 23:40:42 +0400

    First commit

---
+++ src/components/PopularMovies.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container bg-secondary text-white">
+    Hi
+    <i class="fab fa-pinterest-square"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PopularMovies",
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,23 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import bootstrap from "bootstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).use(store).use(router).mount("#app");
+library.add(faUserSecret);
+
+import "@popperjs/core";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(VueAxios, axios)
+  .use(bootstrap)
+  .use(FontAwesomeIcon)
+  .mount("#app");
